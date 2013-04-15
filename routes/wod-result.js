5e8b488783a91312a7b3b21b5115fdb61c5163c5
Victor Powell <vicapow@gmail.com>
2013-04-15 09:09:14 -0700

    fixed bug with missing workout date

--- routes/wod-result.js
+++ routes/wod-result.js
@@ -5,11 +5,12 @@ module.exports = function(app){
   app.post('/wod-result/create', function(req, res, next){
     if(!req.isAuthenticated()) return res.redirect('/')
     var workout = JSON.parse(req.body.workout)
-    if(workout.date === 'today') 
-      workout.date = new Date()
-    if(workout.date === 'yesterday') 
-      workout.date = new Date(new Date() - 24 * 60 * 60 * 1000)
-    else workout.date = null
+    var date = workout.date
+    if(date === 'today') date = new Date()
+    else if(date === 'yesterday') 
+      date = new Date(new Date() - 24 * 60 * 60 * 1000)
+    else date = null
+    workout.date = date
     workout = new Workout(workout)
     workout.creator = req.user._id
     workout.save()
